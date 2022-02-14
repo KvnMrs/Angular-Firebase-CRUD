fd8d97b7171184c2c50229b7c408e4efb436408e
Kévin Marais <kevin.mrs2020@gmail.com>
2022-02-14 12:21:50 +0100

    connect Angular project to Firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
 
+import { environment } from '../environments/environment';
+import { FormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideFirestore(() => getFirestore())
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'video-games-b36e4',
+    appId: '1:627992524369:web:80c3f91b85b0dd4841aa70',
+    storageBucket: 'video-games-b36e4.appspot.com',
+    locationId: 'europe-west',
+    apiKey: 'AIzaSyAebLtpiOB8U5PZ1GOAOebVbSt-8o3keog',
+    authDomain: 'video-games-b36e4.firebaseapp.com',
+    messagingSenderId: '627992524369',
+  },
   production: false,
   firebaseConfig : {
     apiKey: "AIzaSyAebLtpiOB8U5PZ1GOAOebVbSt-8o3keog",
