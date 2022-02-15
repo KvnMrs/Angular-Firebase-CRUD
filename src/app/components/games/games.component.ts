78d9dab871782fb30fecc265a1a6789862fa72bf
Kévin Marais <kevin.mrs2020@gmail.com>
2022-02-15 18:01:51 +0100

    refact : delete dead code and correct interface propriety

--- src/app/components/games/games.component.ts
+++ src/app/components/games/games.component.ts
@@ -1,25 +1,19 @@
 import { Component, OnInit } from '@angular/core';
-import { IGame } from 'src/app/models/game.model';
 import { GameService } from 'src/app/services/game.service';
 import { DocumentData } from 'firebase/firestore/lite';
-
 @Component({
   selector: 'app-games',
   templateUrl: './games.component.html',
   styleUrls: ['./games.component.css']
 })
 export class GamesComponent implements OnInit {
-  // games: IGame[] = []
 
   constructor(private gameService : GameService) { }
 
   public listGames: Promise<{ data: DocumentData; id: string }[]> =
   this.gameService.getGames();
 
-  ngOnInit(): void {    
+  ngOnInit(): void {
   }
-
-  
-
 
 }
