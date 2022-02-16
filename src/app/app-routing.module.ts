2d446a4667bb49b61ed2af6fba979f9e02193f56
Kévin Marais <kevin.mrs2020@gmail.com>
2022-02-17 00:03:33 +0100

    route not required remove

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './components/home/home.component';
 import { GamesComponent } from './components/games/games.component';
 import { GameComponent } from './components/game/game.component';
 
 const routes: Routes = [
-  {path: '', component : HomeComponent},
-  {path: 'games', component : GamesComponent},
+  {path: '', component : GamesComponent},
   {path: 'addGame', component : GameComponent}
 ];
 
