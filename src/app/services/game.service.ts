3610770ee709be2e54b5e60ddbbc8b8a2b5a42de
Kévin Marais <kevin.mrs2020@gmail.com>
2022-02-16 20:50:37 +0100

    feat : gameById service

--- src/app/services/game.service.ts
+++ src/app/services/game.service.ts
@@ -20,6 +20,11 @@ export class GameService {
     return collectionData(gameRef, { idField: 'id' }) as Observable<IGame[]>;
   }
 
+  getgameByID(id: string) {
+    const gameRef = doc(this.firestore, `Games/${id}`);
+    return docData(gameRef, { idField: 'id' }) as Observable<IGame>;
+  }
+
 form = new FormGroup({
   name: new FormControl('', Validators.required),
   img_url: new FormControl('', Validators.required),
@@ -31,5 +36,7 @@ public addGame(game: IGame) {
   const gamesRef = collection(this.firestore, 'Games');
   return addDoc(gamesRef, game);
 }
+
+
 
 }
