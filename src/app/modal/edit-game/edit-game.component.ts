432a1df3c31989232b5850825cb85b367eea46d1
Kévin Marais <kevin.mrs2020@gmail.com>
2022-02-21 21:15:08 +0100

    feat : consume updateGame service

--- src/app/modal/edit-game/edit-game.component.ts
+++ src/app/modal/edit-game/edit-game.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { GameService } from 'src/app/services/game.service';
 
 @Component({
   selector: 'app-edit-game',
@@ -6,10 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit-game.component.css']
 })
 export class EditGameComponent implements OnInit {
+  showSubmitMessage: boolean; 
 
-  constructor() { }
+  constructor(public gameService : GameService) { 
+    this.gameService.form,
+    this.showSubmitMessage = false;
+  }
 
   ngOnInit(): void {
   }
 
+  onSubmit() {
+    const data = this.gameService.form.value;
+    this.gameService.updateGame(data)
+    .then(res => {
+      this.showSubmitMessage = true
+    })}
 }
