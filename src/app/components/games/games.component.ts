a74ef7195c94483dea61b114b3249c27bcb4a268
Kévin Marais <kevin.mrs2020@gmail.com>
2022-02-23 23:35:37 +0100

    style: MAJ

--- src/app/components/games/games.component.ts
+++ src/app/components/games/games.component.ts
@@ -8,12 +8,17 @@ import { IGame } from 'src/app/models/game.model';
 })
 export class GamesComponent implements OnInit {
   games: IGame[] = [];
+showUnderscore = true
+   
 
   constructor(private gameService : GameService) { }
 
   ngOnInit(): void {
     this.gameService.getGames().subscribe((res: IGame[]) => {
       this.games = res;
+      setInterval(() => {
+        this.showUnderscore  = !this.showUnderscore
+      }, 1000)
 
   })
 
@@ -23,4 +28,6 @@ deleteGame(game: IGame) {
     this.gameService.deleteGame(game).then(() => 
      console.log('delete successful'));
   }}
+
+  
 }
